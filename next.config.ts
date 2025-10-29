9f0cb92281aa4af3098807cc984c21eb77ba8752
Raffa <raffaraw@MacBook-Pro-de-Raffa-2.local>
2025-10-29 09:53:30 -0500

    Resolve merge conflict in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,4 @@
-<<<<<<< HEAD
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    eslint:  {
-        ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
->>>>>>> f32420de54c1da8f103e4396c3a340266d9e7891
